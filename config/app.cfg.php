519e7ff5dcb071e06c6c24f60f24dbc6985fa663
HelloDenor <76771741+projectteambition@users.noreply.github.com>
2020-12-31 14:31:54 +0800

    Create app.cfg.php

---
+++ config/app.cfg.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'teambition' => [
+        'cookie' => getenv("cookie"),
+        'projectId' => getenv("projectId"),
+        'maxCount' => 1000
+    ],
+    'title' => getenv("title"),
+    'type' => getenv("type"),
+    'url' => getenv("url"),
+];
+?>
